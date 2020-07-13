e65759bedef9bbd904a15549a868de96f8529494
Chay Williams <Chayvw@gmail.com>
2020-07-13 18:21:12 -0400

    added db and ran it on workbench

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO burgers (name, devoured) VALUES ('American Classic', flase);
+INSERT INTO burgers (name,devoured) VALUES ('BBOY Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Impossible Burger', true);
+
